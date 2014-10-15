40cd6cf4e7063f3202e83c639aa5bfeac3fde0de
hilman <hilmanbeyri@gmail.com>
2014-10-14 21:07:13 -0400

    - add textures from chuan

---
+++ project/Assets/CameraMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	GameObject player;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+}
