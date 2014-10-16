e7273fefbae1793ad46c0262b5272efaba9c5b37
hilman <hilmanbeyri@gmail.com>
2014-10-16 16:50:32 -0400

    - preparation for guard AI

--- project/Assets/Scripts/Player.cs
+++ project/Assets/Scripts/Player.cs
@@ -7,9 +7,11 @@ public class Player : MonoBehaviour {
 	public Texture2D standTexture;
 	public Texture2D[] walkHorTextures;
 	public float walkHorFrameTime;
-	public Texture2D[] walkVerTextures;
-	public float walkVerFrameTime;
-
+	public Texture2D[] walkUpTextures;
+	public float walkUpFrameTime;
+	public Texture2D[] walkDownTextures;
+	public float walkDownFrameTime;
+	
 	bool simulateWithKeyboard;
 	Material playerMat;
 	float animTime;
@@ -17,14 +19,18 @@ public class Player : MonoBehaviour {
 	int prevWalkingDir;
 	int frameIndex;
 	float frameTime;
-	
+	Texture2D[] walkTextures;
+	GameObject graphic;
+
 	void Awake()
 	{
 		GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
 		simulateWithKeyboard = gameController.GetComponent<GameController>().simulateWithKeyboard;
 		playerMat = GetComponentInChildren<MeshRenderer>().material;
+		graphic = transform.Find("Graphic").gameObject;
 		prevWalkingDir = -1;
 		walkingDir = -1;
+		walkTextures = walkHorTextures;
 	}
 
 	void Update()
@@ -32,9 +38,24 @@ public class Player : MonoBehaviour {
 		if(prevWalkingDir != walkingDir)
 		{
 			animTime = 0;
+			frameIndex = 0;
 			if(walkingDir == 2 || walkingDir == 3)
 			{
 				frameTime = walkHorFrameTime;
+				walkTextures = walkHorTextures;
+				playerMat.mainTexture = walkHorTextures[0];
+			}
+			else if(walkingDir == 0)
+			{
+				frameTime = walkUpFrameTime;
+				walkTextures = walkUpTextures;
+				playerMat.mainTexture = walkUpTextures[0];
+			}
+			else if(walkingDir == 1)
+			{
+				frameTime = walkDownFrameTime;
+				walkTextures = walkDownTextures;
+				playerMat.mainTexture = walkDownTextures[0];
 			}
 			else if(walkingDir == -1)
 			{
@@ -48,9 +69,9 @@ public class Player : MonoBehaviour {
 			if(animTime >= frameTime)
 			{
 				animTime -= frameTime;
-				frameIndex = (frameIndex + 1) % walkHorTextures.Length;
+				frameIndex = (frameIndex + 1) % walkTextures.Length;
 
-				playerMat.mainTexture = walkHorTextures[frameIndex];
+				playerMat.mainTexture = walkTextures[frameIndex];
 			}
 		}
 	}
@@ -59,36 +80,63 @@ public class Player : MonoBehaviour {
 	{
 		prevWalkingDir = walkingDir;
 		walkingDir = -1;
+		Vector3 newPos = transform.position;
 
 		if(simulateWithKeyboard)
 		{
 			//up
 			if(Input.GetKey(KeyCode.W))
 			{
-				rigidbody.MovePosition(transform.position + transform.forward * speed * Time.fixedDeltaTime);
-				walkingDir = 0;
+				MoveUp(ref newPos);
 			}
 			//down
 			else if(Input.GetKey(KeyCode.S))
 			{
-				rigidbody.MovePosition(transform.position + -transform.forward * speed * Time.fixedDeltaTime);
-				walkingDir = 1;
+				MoveDown(ref newPos);
 			}
 
 			//right
 			if(Input.GetKey(KeyCode.D))
 			{
-				rigidbody.MovePosition(transform.position + transform.right * speed * Time.fixedDeltaTime);
-				walkingDir = 2;
+				MoveRight(ref newPos);
 			}
 			//left
 			else if(Input.GetKey(KeyCode.A))
 			{
-				rigidbody.MovePosition(transform.position + -transform.right * speed * Time.fixedDeltaTime);
-				walkingDir = 3;
+				MoveLeft(ref newPos);
 			}
-
 		}
+
+		if(walkingDir != -1)
+		{
+			rigidbody.MovePosition(newPos);
+		}
+	}
+
+	void MoveUp(ref Vector3 pos)
+	{
+		pos = pos + transform.forward * speed * Time.fixedDeltaTime;
+		walkingDir = 0;
+	}
+
+	void MoveDown(ref Vector3 pos)
+	{
+		pos = pos + -transform.forward * speed * Time.fixedDeltaTime;
+		walkingDir = 1;
+	}
+
+	void MoveRight(ref Vector3 pos)
+	{
+		pos = pos + transform.right * speed * Time.fixedDeltaTime;
+		walkingDir = 2;
+		graphic.transform.localScale = new Vector3(1, 1, 1);
+	}
+
+	void MoveLeft(ref Vector3 pos)
+	{
+		pos = pos + -transform.right * speed * Time.fixedDeltaTime;
+		walkingDir = 3;
+		graphic.transform.localScale = new Vector3(-1, 1, 1);
 	}
 	
 }
