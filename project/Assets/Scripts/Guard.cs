966318ee010d508cda96bfcacad7365bc8802558
bysreg <hilman09121991@gmail.com>
2014-10-16 10:12:18 -0400

    - changed player movement codes - added guard detection codes

--- project/Assets/Scripts/Guard.cs
+++ project/Assets/Scripts/Guard.cs
@@ -3,6 +3,53 @@ using System.Collections;
 
 public class Guard : MonoBehaviour {
 
+	public float fovAngle = 110f; //degrees that the guard can see
 
+	GameObject player;
+	bool playerInSight;
+	SphereCollider sphereCol;
+	LastPlayerSighting lastPlayerSighting;
 
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		sphereCol = GetComponent<SphereCollider>();
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInSight = false;
+
+			Vector3 direction = player.transform.position - transform.position;
+			float angle = Vector3.Angle(direction, transform.forward);
+
+			if(angle < fovAngle * 0.5f)
+			{
+				RaycastHit hit;
+
+				if(Physics.Raycast(transform.position, direction.normalized, out hit, sphereCol.radius))
+				{
+					if(hit.collider.gameObject == player)
+					{
+						playerInSight = true;
+					}
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInSight = false;
+		}
+	}
+
+	void FixedUpdate()
+	{
+
+	}
 }
