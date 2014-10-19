8944dc6d8f5d850e570e3299ae76b41536f97297
bysreg <hilman09121991@gmail.com>
2014-10-19 13:51:08 -0400

    + SceneFader (not finished)

---
+++ project/Assets/Scripts/SceneFader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFader : MonoBehaviour {
+
+	public bool isFading;
+	public float targetAlpha;
+
+	void Awake()
+	{
+		guiTexture.pixelInset = new Rect(0, 0, Screen.width, Screen.height);
+		guiTexture.enabled = false;
+	}
+
+	void Update()
+	{
+		if(isFading)
+		{
+
+		}
+	}
+
+	public void FadeInScene()
+	{
+		guiTexture.enabled = true;
+		isFading = true;
+		targetAlpha = 1f;
+	}
+
+	public void FadeOutScene()
+	{
+		guiTexture.enabled = true;
+		isFading = true;
+		targetAlpha = 0f;
+	}
+
+}
