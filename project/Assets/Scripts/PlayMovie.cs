2c9362dbba47d2ff10e4336b4d0ab596dd040534
hilman <hilmanbeyri@gmail.com>
2014-10-19 17:34:55 -0400

    - added scenes for playing movie for the story

---
+++ project/Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovie : MonoBehaviour {
+
+	public Object nextScene;
+
+	MovieTexture movie;
+
+	void Start()
+	{
+		movie = renderer.material.mainTexture as MovieTexture;
+		movie.Play();
+	}
+
+	void Update()
+	{
+		if(!movie.isPlaying)
+		{
+			if(nextScene != null)
+				Application.LoadLevel(nextScene.name);
+		}
+	}
+
+}
