38f925b660a8936251742d41123eb1456fc01940
jerrytjohn <jerrytjohn@gmail.com>
2014-10-19 21:40:24 -0400

    Added sound

---
+++ project/Assets/Scripts/VolumeRolloff.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeRolloff : MonoBehaviour 
+{
+	public GameObject Listener;
+	public AudioSource Source;
+	public float Dist;
+	public float k;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Listener = GameObject.FindGameObjectsWithTag ("PsuedoListener");
+		Source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Dist = Vector3.Magnitude(transform.position - Listener.transform.position);
+		Source.volume = Mathf.Pow (0.95f, k * Dist*Dist);
+	
+	}
+}
