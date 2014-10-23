190a0460e9699bf26e8ed399b495df968bd0e9a9
hilman <hilmanbeyri@gmail.com>
2014-10-23 15:09:23 -0400

    + phidget test success

---
+++ project/Assets/Scripts/PhidgetTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Phidgets;
+
+public class PhidgetTest : MonoBehaviour {
+	
+	InterfaceKit ifk;
+	
+	void Start()
+	{
+		ifk = new InterfaceKit();
+		ifk.open();
+		ifk.waitForAttachment();
+		print ("done");
+	}
+	
+	void Update()
+	{
+		int sensor1 = ifk.sensors[0].Value;
+		int sensor2 = ifk.sensors[6].Value;
+		
+		print (sensor1 + " " + sensor2);
+	}
+}
